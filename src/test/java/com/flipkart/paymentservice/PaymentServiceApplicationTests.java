448d7b6c0ce7ba3bc6d087655a5f562967ff46f8
harshan-mj <harshan845@gmail.com>
2026-05-30 22:34:07 +0530

    my last commit for today

--- src/test/java/com/flipkart/paymentservice/PaymentServiceApplicationTests.java
+++ src/test/java/com/flipkart/paymentservice/PaymentServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.flipkart.paymentservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PaymentServiceApplication.class)
 class PaymentServiceApplicationTests {
 
     @Test
